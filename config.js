22367c2c64a5f13bd87c1b710e5237272cea4d11
luckypoem <luckypoem@gmail.com>
2024-12-07 23:14:40 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 export default {
   // Github username
-  username: "bufgix",
+  username: "luckypoem",
   // Repo name where you will write your blogs
-  repoName: "bufgix.github.io",
+  repoName: "github-blog-by-bufgix",
   // If you want, you can automatically add a CV that based 
   // on your github profile. It will appear on the homepage
-  useGithubCv: true
+  useGithubCv: false
 };
